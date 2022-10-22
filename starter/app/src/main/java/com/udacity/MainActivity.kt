3a506b2a01d542046a5cf0d46660b8c7dfe96839
hanansabry <tweety624>
2022-10-22 03:53:27 +0200

    solve gradle files problem, let's start coding

--- starter/app/src/main/java/com/udacity/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/MainActivity.kt
@@ -11,9 +11,7 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
-
+import com.udacity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,14 +23,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setSupportActionBar(binding.toolbar)
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
-        custom_button.setOnClickListener {
-            download()
-        }
+//        binding.custom_button.setOnClickListener {
+//            download()
+//        }
     }
 
     private val receiver = object : BroadcastReceiver() {
