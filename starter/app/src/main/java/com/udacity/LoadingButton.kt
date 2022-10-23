5a794633bb8e15812ca06bb8a76a8a4552fcda8e
hanansabry <tweety624>
2022-10-23 23:22:55 +0200

    resubmit try#2

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -4,44 +4,48 @@ import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+    private var sweepAngle: Int = 0
     private var widthSize = 0
     private var heightSize = 0
-    private var buttonBackgroundColor = 0
-    private var buttonTextColor = 0
-    private var buttonLoadingColor = 0
-    private var buttonCircleColor = 0
+    private var buttonText = resources.getString(R.string.button_name);
 
-    private var buttonTextStr = ""
-    private var progress = 0
+    private val loadingCirclePaint: Paint = Paint()
+    private val buttonPaint: Paint = Paint()
+    private val loadingBarPaint: Paint = Paint()
+    private val textPaint: Paint = Paint()
 
+    var loadingDuration: Long = 1000;
 
-    private val valueAnimator = ValueAnimator.ofInt(0, 360).setDuration(2500)
-
-    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        style = Paint.Style.FILL
-        textAlign = Paint.Align.CENTER
-        textSize = 64.0f
+    private val valueAnimator = ValueAnimator.ofInt(0, 360).apply {
+        duration = loadingDuration
+        addUpdateListener { valueAnimator ->
+            sweepAngle = valueAnimator.animatedValue as Int
+            invalidate()
+        }
+        repeatCount = ValueAnimator.INFINITE
     }
 
-    var buttonState: ButtonState by Delegates.observable(ButtonState.Completed) { p, old, new ->
+    var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
         when (new) {
             ButtonState.Loading -> {
-                buttonTextStr = resources.getString(R.string.button_loading)
+                buttonText = resources.getString(R.string.button_loading)
                 valueAnimator.start()
                 invalidate()
             }
             ButtonState.Completed -> {
-                buttonTextStr = resources.getString(R.string.button_name)
+                buttonText = resources.getString(R.string.button_name)
                 valueAnimator.cancel()
-                progress = 0
+                sweepAngle = 0
                 invalidate()
             }
             else -> invalidate()
@@ -50,46 +54,63 @@ class LoadingButton @JvmOverloads constructor(
 
 
     init {
-        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
-            buttonBackgroundColor = getColor(R.styleable.LoadingButton_backgroundColor, 0)
-            buttonTextColor = getColor(R.styleable.LoadingButton_textColor, 0)
-            buttonLoadingColor = getColor(R.styleable.LoadingButton_loadingColor, 0)
-            buttonCircleColor = getColor(R.styleable.LoadingButton_circleColor, 0)
-        }
+        loadingCirclePaint.style = Paint.Style.FILL
+        loadingCirclePaint.color = ContextCompat.getColor(context, R.color.colorPrimaryDark)
+        loadingCirclePaint.isAntiAlias = true
 
-        buttonState = ButtonState.Completed
-        valueAnimator.apply {
-            addUpdateListener {
-                progress = it.animatedValue as Int
-                invalidate()
-            }
-            repeatCount = ValueAnimator.INFINITE
-            repeatMode = ValueAnimator.RESTART
+        loadingBarPaint.style = Paint.Style.FILL
+        loadingBarPaint.color = ContextCompat.getColor(context, R.color.colorPrimary)
+
+        textPaint.textSize = 75f
+        textPaint.textAlign = Paint.Align.CENTER
+
+        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
+            val backgroundColor = getColor(R.styleable.LoadingButton_backgroundColor, 0)
+            buttonPaint.style = Paint.Style.FILL
+            buttonPaint.color = backgroundColor
+
+            val buttonTextColor = getColor(R.styleable.LoadingButton_textColor, 0)
+            textPaint.color = buttonTextColor
         }
     }
 
+    private val rect: RectF = RectF(0f, 0f, 0f, 0f)
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        paint.color = buttonBackgroundColor
-        canvas?.drawRect(0f, 0f, widthSize.toFloat(), heightSize.toFloat(), paint)
+        //draw the button
+        canvas?.drawRect(0f, 0f, widthSize.toFloat(), heightSize.toFloat(), buttonPaint)
 
-        paint.color = buttonLoadingColor
-        canvas?.drawRect(0f, 0f, widthSize * progress / 360f, heightSize.toFloat(), paint)
-
-        paint.color = buttonTextColor
-        canvas?.drawText(buttonTextStr, widthSize / 2.0f, heightSize / 2.0f + 30.0f, paint)
-
-        paint.color = buttonCircleColor
-        canvas?.drawArc(
-            widthSize - 200f,
-            50f,
-            widthSize - 100f,
-            150f,
+        //draw the loading bar
+        canvas?.drawRect(
             0f,
-            progress.toFloat(),
+            0f,
+            widthSize.toFloat() * sweepAngle / 360,
+            heightSize.toFloat(),
+            loadingBarPaint
+        )
+
+        val circleRadius = 100f
+        val circleLeft = (widthSize.toFloat() - widthSize / 5)
+        val circleRight = circleLeft + circleRadius
+        val circleTop = 50
+        val circleBottom = circleTop + circleRadius
+        canvas?.drawArc(
+            circleLeft,
+            circleTop.toFloat(),
+            circleRight,
+            circleBottom,
+            0f,
+            sweepAngle.toFloat(),
             true,
-            paint
+            loadingCirclePaint
+        )
+
+        canvas?.drawText(
+            buttonText,
+            widthSize.toFloat() / 2,
+            25 + heightSize.toFloat() / 2,
+            textPaint
         )
 
     }
